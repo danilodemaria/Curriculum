54cc5e0db4b96fb8cf25a2a8508b5d29a63621bb
Danilo de Maria <danilo@cheap2ship.com>
2020-07-27 18:38:55 -0300

    dale

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.post('pull', (req,res) => {
+app.post('/pull', (req,res) => {
     console.log('🚀️😁🚀️ Server Online on',PORT)
     exec("git pull", (error, stdout, stderr) => {
         if (error) {
